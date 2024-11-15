c1cedd831c98120b5e463f52af3a7839cd243c21
Saharsh1005 <saharshbarve3@gmail.com>
2024-11-15 16:19:48 -0600

    fix: clean code, modularize the logic

--- src/strategies/load_prompt_template.py
+++ src/strategies/load_prompt_template.py
@@ -7,6 +7,40 @@ def load_prompt_structure(file_path: str = 'src/strategies/prompt_templates.yaml
     with open(file_path, 'r') as file:
         return yaml.safe_load(file)
 
+# ================== Preprocessing functions ==================
+def zero_shot_preprocess(template: str, examples: list = None) -> str:
+    """
+    Preprocess the question for the zero-shot strategy.
+    """
+    return template
+
+def few_shot_preprocess(template: str, examples: list = None) -> str:
+    """
+    Preprocess the question for the few-shot strategy.
+    """
+    if "{{#examples}}" in template:
+        example_str = "\n\n".join(
+            f"Q: {ex['input']}\nA: {ex['output']}" for ex in examples
+        )
+        template_ = template.replace("{{#examples}}", example_str)
+    else:
+        template_ = template
+
+    return template_
+
+def cot_preprocess(template: str, examples: list = None) -> str:
+    """
+    Preprocess the question for the COT strategy.
+    """
+    return template
+
+def self_consistency_cot_preprocess(template: str, examples: list = None) -> str:
+    """
+    Preprocess the question for the self-consistency COT strategy.
+    """
+    return template
+# ================================================================
+
 def generate_prompt(strategy: str, question: str) -> str:
     """
     Generate a prompt based on the selected strategy.
@@ -22,15 +56,10 @@ def generate_prompt(strategy: str, question: str) -> str:
     template = strategy_config['template']
     examples = strategy_config.get('examples', [])
     
-    # Handle the few-shot example substitution
-    if "{{#examples}}" in template:
-        example_str = "\n\n".join(
-            f"Q: {ex['input']}\nA: {ex['output']}" for ex in examples
-        )
-        prompt = template.replace("{{#examples}}", example_str)
-    else:
-        prompt = template
+    # Get the preprocess function based on the strategy
+    prompt = globals()[f"{strategy}_preprocess"](template, examples)
     
+    # Replace the question placeholder with the actual question
     prompt = prompt.replace("{{question}}", question)
     
     return prompt
@@ -42,4 +71,4 @@ if __name__ == "__main__":
     
     for i,strategy in enumerate(strategies):
         prompt = generate_prompt(strategy, question)
-        print(f"{i}.Generated prompt for {strategy} strategy:\n{prompt}\n")
+        print(f"{i}.Generated prompt for {strategy} strategy:\n\n{prompt}\n")
